6b4ad5e087acedd9209742255b3843868e57b6cb
Ferorg <123727750+Ferorg@users.noreply.github.com>
2023-05-11 18:27:49 +0300

    hh

--- src/ExceptionRun.java
+++ src/ExceptionRun.java
@@ -1,17 +1,3 @@
 public class ExceptionRun extends RuntimeException {
-    private String code;
 
-
-    public ExceptionRun(String message, String code) {
-        super(message);
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
--- src/MyException.java
+++ src/MyException.java
@@ -1,16 +1,3 @@
 public class MyException extends Exception {
-    private String code;
 
-
-    public MyException(String message, String code) {
-        super(message);
-        this.code = code;
-    }
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
